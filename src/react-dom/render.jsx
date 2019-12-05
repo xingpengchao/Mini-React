209cf152cbc7cb89bef8fe2341b3a3d38f7967ab
XPC2659489932 <2659489932@qq.com>
2019-12-05 18:07:29 +0800

    实现组件

--- src/react-dom/render.jsx
+++ src/react-dom/render.jsx
@@ -2,7 +2,34 @@ import setAttribute from './dom.jsx';
 
 // render方法的作用是将虚拟DOM渲染成真实DOM
 // render方法接收两个参数，第一参数为虚拟DOM，第二个参数为所要挂载的的真实目标DOM
-const render = ( vnode, container ) => {
+const render = (vnode, container) => {
+    // 将渲染结果挂载到真正的DOM上
+    return container.appendChild(_render(dom))
+} 
+
+const _render = ( vnode, container ) => {
+    // 当虚拟DOM未定义，终止函数
+    if ( vnode === undefined ) return;
+    
+    // 当虚拟DOM是组件
+    if ( vnode.isReactComponent ) {
+        const component = vnode;
+        // 判断组件是否已挂载在DOM上
+        if ( component._container ) {
+            // 组件已挂载且有componentWillUpdate()方法，调用方法
+            if ( component.componentWillUpdate ) {
+                component.componentWillUpdate();
+            }
+            // 组件未挂载且有componentWillMount()方法，调用方法
+        } else if ( component.componentWillMount ) {
+            component.componentWillMount();
+        }
+        
+         // 保存父容器信息，用于更新
+        component._container = container;  
+         
+        vnode = component.render();
+    }
     // 当vnode为字符串时，渲染结果是一段文本
     if ( typeof vnode === 'string' ) {
         // 利用原生DOM操作创建文本节点
@@ -17,17 +44,28 @@ const render = ( vnode, container ) => {
     if ( vnode.attrs ) {
         Object.keys( vnode.attrs ).forEach( key => {
             const value = vnode.attrs[ key ];
-            // 设置属性
-            setAttribute( dom, key, value );   
+            // 如果是属性集合的属性名是类名，直接设置其属性值
+            if ( key === 'className' ) key = 'class';
+
+            // 如果属性值是事件监听函数，则直接附加到dom上
+            if ( typeof value === 'function' ) {
+                dom[ key.toLowerCase() ] = value;
+            } else {
+                // 其他情况调用setAttribute方法设置属性
+                dom.setAttribute( key, vnode.attrs[ key ] );
+            }  
         } );
     }
     
     // 递归渲染子节点
-    vnode.children.forEach( child => render( child, dom ) );   
+    if(vnode.children){
+        vnode.children.forEach( child => render( child, dom ) );   
+    }
     
-    // 将渲染结果挂载到真正的DOM上
-    return container.appendChild( dom );    
+    // 返回真正的DOM
+    return dom;    
 }
+
 
 // 导出render方法接口
 export default render;
---
+++ src/react/component.jsx
@@ -0,0 +1,20 @@
+import { enqueueSetState } from './set-state-queue.jsx';
+
+// 定义组件基类
+class Component{
+	constructor(props ={}){
+		// 用判断是否为类组件
+		this.isReactComponent = true;
+		// 初始化props和state
+		this.props = props;
+		this.state = {};
+	}
+    
+     // 定义异步setState方法，用于改变状态
+	setState(stateChange){
+		enqueueSetState(stateChange, this);
+	}
+}
+
+ // 导出组件基类Component接口 
+export default Component;
