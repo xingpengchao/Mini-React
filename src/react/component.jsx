663685cdadf5541a0ee13902fc5bd2b56f19d7af
XPC2659489932 <2659489932@qq.com>
2020-01-03 17:25:26 +0800

    update

--- src/react/component.jsx
+++ src/react/component.jsx
@@ -1,20 +1,20 @@
 import { enqueueSetState } from './set-state-queue.jsx';
 
-// 定义组件基类
+// 定义React组件基类/父类
 class Component{
 	constructor(props ={}){
 		// 用于判断是否为类组件
 		this.isReactComponent = true;
-		// 初始化props和state
+		// 初始化实例属性props和state
 		this.props = props;
 		this.state = {};
 	}
     
-     // 定义异步setState方法，用于改变状态
+     // 定义原型方法setState，用于改变状态
 	setState(stateChange){
 		enqueueSetState(stateChange, this);
 	}
 }
 
- // 导出组件基类Component接口 
+ // 导出组件基类/父类Component接口 
 export default Component;
--- src/react/create-element.jsx
+++ src/react/create-element.jsx
@@ -1,9 +1,9 @@
 import Component from './component.jsx'
 
-/*createElement方法将JSX编译并返回JS对象，返回的JS对象记录了该标签的所有DOM信息，也就是虚拟DOM。
-createElement方法接收一个三个参数：
+/*createElement方法用于编译JSX并返回JS对象，返回的JS对象记录了该标签的所有DOM信息，也就是虚拟DOM。
+createElement方法接收三个参数：
 第一个参数是DOM节点标签，比如div、h1、span等等;
-第二个参数是一个对象，对象包含了标签的所有属性，比如className、id、style等等；
+第二个参数是一个对象，表示属性集合，包含了标签的所有属性，比如className、id、style等等；
 第三个参数是一个数组，使用扩展运算符将child1、child2一些列子节点合并成的一个数组children；*/
 
 function createElement( tag, attrs, ...children ) {
@@ -15,5 +15,5 @@ function createElement( tag, attrs, ...children ) {
     }
 }
 
-// 导出createElement方法接口
+// 导出createElement接口
 export default createElement;
